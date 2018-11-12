6cc4b31146ddab51f6678f3c02008491e6d7deca
kamszot <wa12rior@gmail.com>
2018-11-12 19:37:51 +0100

    Initial commit

---
+++ src/com/gmail/wa12rior/Rain/Game.java
@@ -0,0 +1,28 @@
+package com.gmail.wa12rior.Rain;
+
+public class Game implements Runnable {
+  public static int width = 300;
+  public static int height = width / 16 * 9;
+  public static int scale = 3;
+
+  private Thread thread;
+  private boolean running = false;
+
+  public synchronized void start() {
+    thread = new Thread(this, "Display");
+    thread.start();
+  }
+
+  public synchronized void stop() {
+    try {
+      thread.join();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public void run() {
+
+  }
+}
